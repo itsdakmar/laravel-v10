b37f8b3d84fb03b957f3718e13ed9acbf94f6b11
Ammar Razaman <52007864+itsdakmar@users.noreply.github.com>
2024-06-21 17:35:31 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
                                     </svg>
                                 </div>
 
-                                <h1 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">KEKASIH KU DI MENARA</h2>
+                                <h1 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">8======D</h2>
 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
